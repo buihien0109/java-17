d9938aa8ed5cf302238590f01a82b7a34f7c08bf
buihien0109 <buihien01091997@gmail.com>
2023-06-21 12:51:17 +0700

    update

---
+++ springboot/jpa/day_04/user-management/src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Course;
+import com.example.demo.model.Topic;
+import com.example.demo.service.CourseService;
+import com.example.demo.service.TopicService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+    private final TopicService topicService;
+
+
+    @GetMapping("/")
+    public String getAllCourse(@RequestParam(required = false, defaultValue = "1") Integer page,
+                               @RequestParam(required = false, defaultValue = "6") Integer pageSize,
+                               @RequestParam(required = false) String topic,
+                               @RequestParam(required = false) String term,
+                               Model model) {
+        Page<Course> pageInfo = courseService.getAllCourse(page, pageSize);
+        List<Topic> topicList = topicService.getAllTopic();
+
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("topicList", topicList);
+        return "web/course-list";
+    }
+
+    @GetMapping("/khoa-hoc/online")
+    public String getOnlineCourse(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                  @RequestParam(required = false, defaultValue = "6") Integer pageSize,
+                                  Model model) {
+        Page<Course> pageInfo = courseService.getAllCourse(page, pageSize, "online");
+        List<Topic> topicList = topicService.getAllTopic();
+
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("topicList", topicList);
+        return "web/course-online-list";
+    }
+
+    @GetMapping("/khoa-hoc/onlab")
+    public String getOnLabCourse(Model model) {
+        List<Course> courseList = courseService.getAllCourse("onlab");
+        List<Topic> topicList = topicService.getAllTopic();
+
+        model.addAttribute("courseList", courseList);
+        model.addAttribute("topicList", topicList);
+        return "web/course-onlab-list";
+    }
+
+    @GetMapping("/khoa-hoc/{id}")
+    public String getDetailCourse(@PathVariable Integer id, Model model) {
+        Course course = courseService.getCourseDetail(id);
+        model.addAttribute("course", course);
+        return "web/detail";
+    }
+}
